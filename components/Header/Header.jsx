bccc9550032eb7c11155ad8b772ea44e177b8f03
anis rbh <mohamedrbh519@gmail.com>
2022-05-04 08:22:10 +0100

    1 - ajout d'un Header

---
+++ components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>Welcome!</Text>
+            <Text style={styles.text}>are you a member? login here!</Text>
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#093357',
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+        borderRadius: 5,
+    },
+    title: {
+        textAlign: 'center',
+        color: '#bbb',
+        fontSize: 32,
+        fontWeight: 'bold',
+    },
+    text: {
+        fontSize: 20,
+        color: '#bbb',
+        textAlign: 'center'
+    }
+})
